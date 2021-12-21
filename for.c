aa1852487def1129326b5ba021317be4fcae7313
Erick Saavedra <>
2021-12-21 16:48:39 -0600

    bucle for

---
+++ for.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int n = 20;
+int i;
+
+int main(){
+
+    for( i = 0; i < n; i++ )
+        printf("el valor actual de i es %d \n", i);
+    printf("se termino el for");
+    
+    return 0;
+}
